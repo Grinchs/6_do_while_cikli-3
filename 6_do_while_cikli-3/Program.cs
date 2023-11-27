c6c84273d4a21be3088aebaa45af32440c2ef069
Grinchs <marcisg2345@gmail.com>
2023-11-27 23:57:11 +0200

    Add project files.

---
+++ 6_do_while_cikli-3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+int R;
+double[] skaitluMasivs;
+double reizinajums = 1;
+
+Console.WriteLine("Ievadiet skaitli R: ");
+while (!int.TryParse(Console.ReadLine(), out R) || R <= 0)
+{
+    Console.WriteLine("Nederīgs skaitlis");
+}
+
+skaitluMasivs = new double[R];
+
+for (int i = 0; i < R; i++)
+{
+    Console.WriteLine($"Ievadiet {i + 1}. skaitli:");
+    while (!double.TryParse(Console.ReadLine(), out skaitluMasivs[i]))
+    {
+        Console.WriteLine("Nederīgs skaitlis. Ievadiet skaitli vēlreiz:");
+    }
+}
+
+for (int i = 0; i < R; i++)
+{
+    if (i == 0)
+    {
+        reizinajums = skaitluMasivs[i];
+        Console.WriteLine($"Reiz[{i}] = {reizinajums}");
+    }
+    else
+    {
+        reizinajums *= skaitluMasivs[i];
+        Console.WriteLine($"Reiz[{i}] = {skaitluMasivs[i - 1]}*{skaitluMasivs[i]}");
+    }
+}
+
+Console.WriteLine($"Reizinājums ir: {reizinajums}");
